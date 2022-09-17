7d140677cbe4fb2296e32596f698551a1cd65d50
thanhhoan@ILA <hoan.phan@ilavietnam.edu.vn>
2022-09-17 08:50:28 +0700

    fix redux-persist related

--- src/utils/helpers/local-storage.helper.js
+++ src/utils/helpers/local-storage.helper.js
@@ -1,3 +1,7 @@
-export const persistRoot = JSON.parse(localStorage.getItem('persist:root'));
-export const persistAuthStatus = JSON.parse(persistRoot.auth).authStatus;
-export const persistUser = JSON.parse(persistRoot?.auth)?.user;
+const isExistKey = localStorage.getItem('persist:root');
+
+export const persistRoot =
+  isExistKey && JSON.parse(localStorage.getItem('persist:root'));
+export const persistAuthStatus =
+  isExistKey && JSON.parse(persistRoot.auth).authStatus;
+export const persistUser = isExistKey && JSON.parse(persistRoot?.auth)?.user;
