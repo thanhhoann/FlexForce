9cb927e592afa8a712d2d46bfaa35423ceb7bfeb
thanhhoan@ILA <hoan.phan@ilavietnam.edu.vn>
2022-12-13 11:33:51 +0700

    update mobile version

--- src/components/MainLayout.js/index.js
+++ src/components/MainLayout.js/index.js
@@ -1,16 +1,43 @@
-import { Box, useColorModeValue } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Text,
+  useColorModeValue,
+  useMediaQuery,
+} from '@chakra-ui/react';
 import ParallaxContainer from '../../utils/Parallax/ParallaxContainer';
 import Footer from '../Footer';
 import NavBar from '../NavBar';
 
 export default function MainLayout({ children }) {
+  const [isSmallerThan768] = useMediaQuery('(max-width: 768px)');
+
   return (
     <>
-      {/* <ParallaxContainer>  */}
-      <NavBar />
-      {children}
-      {/* <Footer /> */}
-      {/* </ParallaxContainer> */}
+      {isSmallerThan768 ? (
+        <Center
+          isCentered
+          bg="rgba( 255, 255, 255, 0.25 )"
+          boxShadow="0 8px 32px 0 rgba( 31, 38, 135, 0.37 )"
+          backdropFilter=" blur( 4px )"
+          rounded="10px"
+          border="1px solid rgba( 255, 255, 255, 0.18 )"
+          w="100vw"
+          h="100vh"
+        >
+          <Text w="15rem" h="8rem">
+            Please view the website on desktop. Thanks bro :)))))
+          </Text>
+        </Center>
+      ) : (
+        <>
+          {/* <ParallaxContainer>  */}
+          <NavBar />
+          {children}
+          {/* <Footer /> */}
+          {/* </ParallaxContainer> */}
+        </>
+      )}
     </>
   );
 }
