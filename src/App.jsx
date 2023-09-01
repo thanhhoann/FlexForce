ad6d67c1a04a780f5b1d3ed0aa18326951a0f550
phandinhthanhhoan <thanhhoan120702@gmail.com>
2023-09-02 00:11:15 +0700

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Box,
   ChakraProvider,
@@ -8,16 +8,16 @@ import {
   Text,
   theme,
   VStack,
-} from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./utils/helpers";
-import { useDispatch, useSelector } from "react-redux";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";
-import { routeActions, routeSelect } from "./slices/routeSlice";
-import { authActions, authSelect, userSelect } from "./slices/authSlice";
-import SignUp from "./pages/signup/SignUp";
-import SignIn from "./pages/signin/SignIn";
-import { createStandaloneToast } from "@chakra-ui/toast";
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './utils/helpers';
+import { useDispatch, useSelector } from 'react-redux';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import { routeActions, routeSelect } from './slices/routeSlice';
+import { authActions, authSelect, userSelect } from './slices/authSlice';
+import SignUp from './pages/signup/SignUp';
+import SignIn from './pages/signin/SignIn';
+import { createStandaloneToast } from '@chakra-ui/toast';
 
 export default function App() {
   const dispatch = useDispatch();
@@ -25,7 +25,7 @@ export default function App() {
   const { ToastContainer, toast } = createStandaloneToast();
 
   React.useEffect(() => {
-    const updateUser = onAuthStateChanged(auth, (currentUser) => {
+    const updateUser = onAuthStateChanged(auth, currentUser => {
       dispatch(authActions.setUser(currentUser));
     });
     return () => {
